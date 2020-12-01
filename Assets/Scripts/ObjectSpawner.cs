8494327f9338984f3092286adfa287dd76777611
fsdf3rft <fsdfsd@sdfsd>
2020-12-01 12:26:16 +0300

    add start project

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+
+    private PlacmentIndicator placmentIndicator;
+
+    private void Start()
+    {
+        placmentIndicator = FindObjectOfType<PlacmentIndicator>();
+    }
+
+    private void Update()
+    {
+        if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            GameObject obj = Instantiate(objectToSpawn, placmentIndicator.transform.position, placmentIndicator.transform.rotation);
+        }
+    }
+
+}
